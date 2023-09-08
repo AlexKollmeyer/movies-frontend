407f3e1ec22b01b9a298c144e8718839db0465c6
Alex Kollmeyer <akollmeyer123@gmail.com>
2023-09-08 15:22:15 -0500

    Lifted state to app.jsx

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,6 +1,7 @@
 import MovieItem from "../MovieItem/MovieItem";
-const MovieList = ({}) => {
-    const movies=['Princess Bride','Forest Gump','Iron Man','Pulp Fiction']
+    
+    const MovieList = ({movies = []}) => {
+    
     const movieItems =movies.map((movie) => <MovieItem key={movie} title={movie}/>) 
     return ( <div className="flex-item">
         <h4>My Movies</h4>
