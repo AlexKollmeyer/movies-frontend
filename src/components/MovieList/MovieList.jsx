6db7b39e38d9b1959af3df7ecf4aa3ce357fcffe
Alex Kollmeyer <akollmeyer123@gmail.com>
2023-09-08 16:38:23 -0500

    Did prop drilling

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,8 +1,16 @@
 import MovieItem from "../MovieItem/MovieItem";
     
-    const MovieList = ({movies = []}) => {
+    const MovieList = ({movies = [], activeIndex, setActiveIndex}) => {
     
-    const movieItems =movies.map((movie) => <MovieItem key={movie.title} title={movie.title}/>) 
+    const movieItems =movies.map((movie, i) => (
+        <MovieItem 
+            key={movie.title} 
+            title={movie.title} 
+            activeIndex={activeIndex} 
+            setActiveIndex={setActiveIndex}
+            index={i}
+        />
+    ));
     return ( <div className="flex-item">
         <h4>My Movies</h4>
         <div >
