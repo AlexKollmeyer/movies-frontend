d07aad86a4bef8cdc6f30487a7720e9144b941a0
Alex Kollmeyer <akollmeyer123@gmail.com>
2023-09-13 15:38:47 -0500

    Finished the Post functionality.

--- src/App.jsx
+++ src/App.jsx
@@ -23,11 +23,6 @@ function App() {
     fetchMovies();
   }, []);
 
-  const handleNewMovie = (newMovie) => {
-    const updatedMovies = [...movies, newMovie];
-    setMovies(updatedMovies);
-  };
-
   const selectedMovie = movies[activeIndex];
 
   return (
@@ -40,7 +35,7 @@ function App() {
           setActiveIndex={setActiveIndex}
         />
         <MovieInfo movieObj={selectedMovie} />
-        <NewMovieForm onNewMovie={handleNewMovie} />
+        <NewMovieForm onNewMovie={fetchMovies} />
       </div>
     </div>
   );
--- src/components/NewMovieForm/NewMovieForm.jsx
+++ src/components/NewMovieForm/NewMovieForm.jsx
@@ -1,31 +1,46 @@
-import React, { useState } from 'react';
-import TextField from '../TextField/TextField';
+import React, { useState } from "react";
+import TextField from "../TextField/TextField";
+import axios from "axios";
 
-const NewMovieForm = ({onNewMovie}) => {
-    const [title, setTitle] = useState('');
-    const [runningTime, setRunningTime] = useState('');
-    const [genre, setGenre] = useState('');
+const NewMovieForm = ({ onNewMovie }) => {
+  const [title, setTitle] = useState("");
+  const [runningTime, setRunningTime] = useState("");
+  const [genre, setGenre] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        const formData = {
-            title,
-            runningTime,
-            genre
-        };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = {
+      title,
+      runningTime,
+      genre,
+    };
 
-        onNewMovie(formData)
+    try {
+      const response = await axios.post(
+        "http://localhost:5205/api/Movies",
+        formData
+      );
+      if (response.status === 201) {
+        onNewMovie();
+      }
+    } catch (error) {
+      console.warn("Error submitting new movie form:", error);
     }
+  };
 
+  return (
+    <form onSubmit={handleSubmit} className="flex-item">
+      <h4>Add Movie</h4>
+      <TextField label="Title" value={title} onChange={setTitle} />
+      <TextField
+        label="RunningTime"
+        value={runningTime}
+        onChange={setRunningTime}
+      />
+      <TextField label="Genre" value={genre} onChange={setGenre} />
+      <button type="submit">Add Movie</button>
+    </form>
+  );
+};
 
-
-    return ( <form onSubmit={handleSubmit} className="flex-item">
-        <h4>Add Movie</h4>
-        <TextField label='Title' value={title} onChange={setTitle} />
-        <TextField label='RunningTime' value={runningTime} onChange={setRunningTime} />
-        <TextField label='Genre' value={genre} onChange={setGenre} />
-        <button type='submit'>Add Movie</button>
-    </form> );
-}
- 
 export default NewMovieForm;
